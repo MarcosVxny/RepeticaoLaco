32d845398f6ef6c50615bbb9edda652505dd72f2
Marcos Vinicius <83991350+MarcosVxny@users.noreply.github.com>
2023-02-20 15:30:25 -0300

    Update Exer1.java

--- Exer1.java
+++ Exer1.java
@@ -5,6 +5,23 @@ public class Exer1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int x;
+		
+		for(x=1000; x<=1999; x++) {
+			
+			if(x%11==5) {
+				System.out.print("\n"+x);
+				
+				
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
 	}
 
 }
